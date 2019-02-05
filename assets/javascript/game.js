7f07451dfdc57fa5925c452e23ba87f49516de72
Rakim francis <rakimfrancis@Rakims-MBP.home>
2019-02-05 10:18:55 -0500

    3_rd

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -74,8 +74,8 @@ function work() {
 
             // keep this variable constant
 
-            var enter_a_letter2 = prompt(
-                "Please enter a letter , if you guess the word correctly, you win!") + prompt(
+            var enter_a_letter2 = prompt((
+                    "Please enter a letter , if you guess the word correctly, you win!") +
                 "This is the number of remaining letters:\n" + answer.join(" "));
             //  + prompt(
             // "hint " +
